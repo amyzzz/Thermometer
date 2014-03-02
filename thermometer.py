ff9d704a2fd5a320dbdef887c2de385ff5dac835
amyzzz <azheng@berkeley.edu>
2014-03-01 20:36:25 -0800

    Update thermometer.py

--- thermometer.py
+++ thermometer.py
@@ -1,34 +1,36 @@
 
+
+
 import csv
 import graphics
 import json
 import urllib.request
 
-class Thermometer(object)
+class Thermometer(object):
 
     color = 'Red'
     url = "http://www.kimonolabs.com/api/6gdqjftg?apikey=0781dcd19879b0e07c2b2f88444c6f0c"
 
     def __init__(self):
-        image = graphics.Canvas(150, 450)
+        self.image = graphics.Canvas(150, 450)
         thermometer = 'therm.gif'
-        image.draw_image((0, 0), thermometer)
+        self.image.draw_image((0, 0), thermometer)
     
-    def find_value(self, str):
-        index = results.index(str)
+    def find_value(self, str, data):
+        index = data.index(str)
         start = index + len(str) + 4
-        end = results.index('"', start)
-        currency = results[start]
-        temp = results[start+1:end]
+        end = data.index('"', start)
+        currency = data[start]
+        temp = data[start+1:end]
         return temp.replace(',', ''), currency
     
 
     
-    def update():
-        results = urllib.request.urlopen(url).read().decode()
+    def update(self):
+        results = urllib.request.urlopen(self.url).read().decode()
 
-        amt_raised = int(find_value("Raised")[0])
-        goal = int(find_value("Goal")[0])
+        amt_raised = int(self.find_value("Raised", results)[0])
+        goal = int(self.find_value("Goal", results)[0])
 
 
         y_bars = amt_raised*335//goal
@@ -38,15 +40,15 @@ class Thermometer(object)
         while (count > (350-y_bars) and count > 15):
             pos = (26, count)
             points = graphics.rectangle_points(pos, 4, 1)
-            image.draw_polygon(points, color, color)
+            self.image.draw_polygon(points, self.color, self.color)
             count -= 1
     
         if y_bars > 335:
             y_bars = 335
-            image.draw_image((0, 0), 'burst.gif')
+            self.image.draw_image((0, 0), 'burst.gif')
   
-        image.draw_text(amt_raised, (60, 350-y_bars))
-        image.draw_text(goal, (5, 405))
+        self.image.draw_text(amt_raised, (60, 350-y_bars))
+        self.image.draw_text(goal, (5, 405))
 
 
         wait = input("PRESS ENTER TO CONTINUE.")
