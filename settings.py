3c90f358a2478e2f6d0c4b6f54decdf29f51bfc1
amyzzz <azheng@berkeley.edu>
2014-03-01 20:25:36 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,3 @@
-
-<script type="text/javascript" src="//sslstatic.wix.com/services/js-sdk/1.24.0/js/Wix.js"></script>
-
 import thermometer
 
 def set_color(new_color):
@@ -11,6 +8,6 @@ def set_url(url):
     
 thermometer = Thermometer()
 
-set_color(input("Set color")
-set_url(input("Set url")
+set_color(input("Set color"))
+set_url(input("Set url"))
 thermometer.update()
